e02161d295f6560793055e6a7f8330c7f7eb1c05
anfego1 <anfego1@hotmail.com>
2024-10-11 10:07:53 +0100

    dominios permitidos en condición

--- configApi.php
+++ configApi.php
@@ -13,5 +13,16 @@ mysqli_report(MYSQLI_REPORT_OFF);
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Content-Type: application/json; charset=utf-8');
-header('Access-Control-Allow-Origin: http://ec2-51-92-121-143.eu-south-2.compute.amazonaws.com/esc-backend/usuarios');
+$allowed_origins = [
+    'https://app-energiasolarcanarias.com',
+    'http://app-energiasolarcanarias.com',
+    'https://localhost:3000',
+    'http://localhost/esc-backend/',
+    // Agrega más dominios permitidos aquí
+];
+
+if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
+    header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+    header('Access-Control-Allow-Credentials: true'); // Opcional si necesitas enviar cookies o autenticación
+}
 
