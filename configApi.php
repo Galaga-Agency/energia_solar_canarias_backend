212e39850eec20e37413fa410f8c714ca6c1e4fb
anfego1 <anfego1@hotmail.com>
2024-10-11 10:13:30 +0100

    cabeceras solicitudes API

--- configApi.php
+++ configApi.php
@@ -9,20 +9,28 @@ ini_set('default_socket_timeout', 7200);
 ini_set("display_errors", 0);
 ini_set("display_startup_errors", 0);
 mysqli_report(MYSQLI_REPORT_OFF);
-
+/*
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Content-Type: application/json; charset=utf-8');
+*/
 $allowed_origins = [
-    'https://app-energiasolarcanarias.com',
-    'http://app-energiasolarcanarias.com',
-    'https://localhost:3000',
-    'http://localhost/esc-backend/',
-    // Agrega más dominios permitidos aquí
+    'http://localhost',  // Durante desarrollo desde localhost
+    'https://app-energiasolarcanarias.com', // Producción
+    // Agrega más dominios permitidos aquí si es necesario
 ];
 
 if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
     header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
-    header('Access-Control-Allow-Credentials: true'); // Opcional si necesitas enviar cookies o autenticación
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization');
+    header('Access-Control-Allow-Credentials: true'); // Solo si necesitas enviar cookies o encabezados de autenticación
+    header('Content-Type: application/json; charset=utf-8');
+}
+
+// Manejo de solicitudes OPTIONS (preflight)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('HTTP/1.1 200 OK');
+    exit();
 }
 
