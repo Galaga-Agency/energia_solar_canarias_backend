37ac9309cd81924b0e397e74d3d8296521aedc8d
Anfego <anfego1@hotmail.com>
2024-11-12 15:55:49 +0000

    Ícono index

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ $page = $_GET['page'] ?? 'inicio';
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>APIREST Documentación - Energía Solar Canarias</title>
-  <link rel="icon" type="image/png" href="./public/assets/img/logo.png">
+  <link rel="icon" type="image/png" href="./public/assets/img/icon.png">
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
   <style>
