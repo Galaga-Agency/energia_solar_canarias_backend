b56872276389f3cb992edd1f5937f232f03de9ad
nestorsan77 <nestorsan77@gmail.com>
2024-11-28 11:22:12 +0100

    energiaConsumption añadida

--- app/services/SolarEdgeService.php
+++ app/services/SolarEdgeService.php
@@ -77,19 +77,64 @@ class SolarEdgeService {
         }
     }
 
-        //Recoge la grafica de la planta
-        public function getPowerConsumption($siteId, $dia, $fechaInicioFormateada = null, $fechaFinFormateada = null) {
-            // Construir la URL utilizando el formato definido
-            $url = $this->solarEdge->getUrl() . "site/$siteId/currentPowerFlow?timeUnit=$dia&startDate=$fechaInicioFormateada&endDate=$fechaFinFormateada&api_key=" . $this->solarEdge->getApiKey();
-        
-            // Realizar la solicitud
-            try {
-                $response = $this->httpClient->get($url);
-                return json_decode($response);
-            } catch (Exception $e) {
-                return ['error' => $e->getMessage()];
-            }
+       //Recoge la grafica de la planta
+    public function getPowerConsumption($siteId, $dia, $fechaFin = null, $fechaInicio = null) {
+        // Formato de fecha
+        $formato = 'Y-m-d H:i:s';
+    
+        // Convertir fechas a DateTime si no son nulas
+        $fechaSinFormatearFin = $fechaFin ? new DateTime($fechaFin) : new DateTime('today 23:59:59');
+        $fechaSinFormatearInicio = $fechaInicio ? new DateTime($fechaInicio) : null;
+    
+        // Ajustar fechas según el valor de $dia
+        switch ($dia) {
+            case "QUARTER_OF_AN_HOUR":
+                // Si no hay fecha de inicio, tomar ayer a las 23:59:59
+                $fechaSinFormatearInicio = $fechaSinFormatearInicio ?? new DateTime('today');
+                break;
+            case "DAY":
+                // Si no hay fecha de inicio, tomar ayer a las 23:59:59
+                $fechaSinFormatearInicio = $fechaSinFormatearInicio ?? new DateTime('yesterday 23:59:59');
+                break;
+    
+            case "WEEK":
+                // Si no hay fecha de inicio, tomar el inicio de la semana actual
+                $fechaSinFormatearInicio = $fechaSinFormatearInicio ?? new DateTime('monday this week 00:00:00');
+                break;
+    
+            case "MONTH":
+                // Si no hay fecha de inicio, tomar el inicio del mes actual
+                $fechaSinFormatearInicio = $fechaSinFormatearInicio ?? new DateTime('first day of this month 00:00:00');
+                break;
+    
+            case "YEAR":
+                // Si no hay fecha de inicio, tomar el inicio del año actual
+                $fechaSinFormatearInicio = $fechaSinFormatearInicio ?? new DateTime('first day of January 00:00:00');
+                break;
+    
+            default:
+                return "Error: día incorrecto";
         }
+
+        //pongo la fecha en formato
+        $fechaSinFormatearInicio->setTime(0, 0, 0); // Establecer la hora como 00:00:00
+        $fechaSinFormatearFin->setTime(23, 59, 59); // Establecer la hora como 23:59:59
+    
+        // Formatear las fechas
+        $fechaInicioFormateada = $fechaSinFormatearInicio->format($formato);
+        $fechaFinFormateada = $fechaSinFormatearFin->format($formato);
+    
+        // Construir la URL utilizando el formato definido
+        $url = $this->solarEdge->getUrl() . "site/$siteId/energyDetails?meters=CONSUMPTION&timeUnit=$dia&startTime=" . urlencode($fechaInicioFormateada) . "&endTime=" . urlencode($fechaFinFormateada) . "&api_key=" . $this->solarEdge->getApiKey();
+        
+        // Realizar la solicitud
+        try {
+            $response = $this->httpClient->get($url);
+            return json_decode($response);
+        } catch (Exception $e) {
+            return ['error' => $e->getMessage()];
+        }
+    }
     
     //Recoge la grafica 'Custom' de la planta
     public function getPowerDashboardCustom($chartField, $foldUp, $timeUnit, $siteId, $billingCycle, $period, $periodDuration, $startTime, $endTime) {
