8556ba204337c5d68f18c8aace0fcf552249ecf5
nestorsan77 <nestorsan77@gmail.com>
2024-11-28 09:57:40 +0100

    Creacion de los bearertoken en la base de datos

--- app/models/SolarEdge.php
+++ app/models/SolarEdge.php
@@ -1,13 +1,24 @@
 <?php
+
+require_once './../controllers/ProveedoresController.php';
+
 class SolarEdge{
     private $url;
     private $api_key;
+    private $proveedoresController;
 
     //definimos el constructor de la clase
     public function __construct()
     {
         $this->url = 'https://monitoringapi.solaredge.com/';
-        $this->api_key = 'TRZUMFRMSND66ZFKV7F47HMBLVQRNQTR';
+        // Inicializar el controlador de proveedores
+        $this->proveedoresController = new ProveedoresController();
+
+        //contiene un diccionario con tokenAuth y con tokenRenovation si estos estan en la bbdd
+        $arrayToken = $this->proveedoresController->getTokenProveedor('SolarEdge');
+
+        // Obtener el token desde la base de datos y asignarlo a api_key
+        $this->api_key = $arrayToken['tokenAuth'];
     }
 
     //definimos el getter y setter
--- app/models/VictronEnergy.php
+++ app/models/VictronEnergy.php
@@ -1,17 +1,29 @@
 <?php
+
+require_once './../controllers/ProveedoresController.php';
+
 class VictronEnergy{
     private $url;
     private $api_key;
     private $id_access_token;
     private $id_installation;
+    private $proveedoresController;
 
     //definimos el constructor de la clase
     public function __construct()
     {
         $this->url = 'https://vrmapi.victronenergy.com/v2/';
-        $this->api_key = 'Token 61ffd7dc0f459c485f527f1056d9492e6418e58adf74cd34be9ce768ac6fe576';
         $this->id_access_token = 2160468;
         $this->id_installation = 58178;
+
+        // Inicializar el controlador de proveedores
+        $this->proveedoresController = new ProveedoresController();
+
+        //contiene un diccionario con tokenAuth y con tokenRenovation si estos estan en la bbdd
+        $arrayToken = $this->proveedoresController->getTokenProveedor('VictronEnergy');
+
+        // Obtener el token desde la base de datos y asignarlo a api_key
+        $this->api_key = $arrayToken['tokenAuth'];
     }
 
     //definimos el getter y setter
