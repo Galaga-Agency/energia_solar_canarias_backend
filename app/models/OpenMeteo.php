f6b0f878c41a7cb7b1c7daeb8510416068e6acbf
nestorsan77 <nestorsan77@gmail.com>
2024-12-05 10:02:54 +0100

    añadido cambios a la recoleccion del clima ahora se puede acceder pasando las coordenadas tambien

--- app/models/OpenMeteo.php
+++ app/models/OpenMeteo.php
@@ -26,5 +26,17 @@ class OpenMeteo{
         }
         return $weatherResponse;
     }
+    public function obtenerClimaCoordenadas($latitude, $longitude){
+        // URL de Open-Meteo para obtener el clima
+        $openMeteoUrl = "https://api.open-meteo.com/v1/forecast?latitude=$latitude&longitude=$longitude&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,rain,showers,snowfall,cloud_cover,wind_speed_10m,wind_direction_10m&hourly=&daily=weather_code,temperature_2m_max,temperature_2m_min,uv_index_max&timezone=Europe/Madrid";
+
+        // Realizar la solicitud a Open-Meteo
+        $weatherResponse = file_get_contents($openMeteoUrl);
+
+        if ($weatherResponse === FALSE) {
+            die("Error al conectarse a Open-Meteo.");
+        }
+        return $weatherResponse;
+    }
 }
 ?>
