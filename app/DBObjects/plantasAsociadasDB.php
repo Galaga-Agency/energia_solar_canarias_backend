2cf0aaf242b010cb75561a97dfd577134484fc88
nestorsan77 <nestorsan77@gmail.com>
2024-12-30 16:23:04 +0100

    Arreglado un bug de los usuarios que hacía que no se mostrase mas de una planta y un bug en la parte de administracion que dejaba los datos a null en la llamada de todas las plantas

--- app/DBObjects/plantasAsociadasDB.php
+++ app/DBObjects/plantasAsociadasDB.php
@@ -2,51 +2,53 @@
 
 require_once './../models/conexion.php';
 
-class PlantasAsociadasDB {
+class PlantasAsociadasDB
+{
     private $conexion;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->conexion = Conexion::getInstance();
     }
 
- /**
-   * Relacionar un usuario con una planta
-    * 
-    * @param int $idPlanta El ID de la planta
-    * @param int $idUsuario El ID del usuario
-    * @param string $proveedor El nombre del proveedor
-    * @return array en caso de éxito o false en caso de error
-    */
-    public function getPlantasAsociadasAlUsuario($idUsuario) {
+    /**
+     * Relacionar un usuario con una planta
+     * 
+     * @param int $idPlanta El ID de la planta
+     * @param int $idUsuario El ID del usuario
+     * @param string $proveedor El nombre del proveedor
+     * @return array en caso de éxito o false en caso de error
+     */
+    public function getPlantasAsociadasAlUsuario($idUsuario)
+    {
         try {
             $conexion = Conexion::getInstance();
             $conn = $conexion->getConexion();
-    
-            $query = "SELECT * FROM plantas_asociadas WHERE usuario_id = ?;";
+
+            $query = "SELECT p.nombre AS nombre_proveedor, pa.planta_id as planta_id FROM plantas_asociadas pa JOIN proveedores p ON pa.proveedor_id = p.id WHERE pa.usuario_id = ?;";
             $stmt = $conn->prepare($query);
             if (!$stmt) {
                 throw new Exception("Error en la preparación de la consulta: " . $conn->error);
             }
-    
+
             // Vincula el parámetro 'i' para enteros
             $stmt->bind_param('i', $idUsuario);
-    
+
             // Ejecuta la consulta
             if (!$stmt->execute()) {
                 throw new Exception("Error en la ejecución de la consulta: " . $stmt->error);
                 return false;
             }
-    
+
             // Recoge los resultados de la consulta
             $result = $stmt->get_result();
             $plantas = [];
             while ($row = $result->fetch_assoc()) {
                 $plantas[] = $row;
             }
-    
+
             // Cierra la consulta y la conexión
             $stmt->close();
-    
             // Devuelve el array de plantas asociadas
             return $plantas;
         } catch (Exception $e) {
@@ -55,48 +57,46 @@ class PlantasAsociadasDB {
         }
     }
     /**
- * Verificar si una planta está asociada a un usuario.
- * 
- * @param int $usuarioId El ID del usuario
- * @param int $idPlanta El ID de la planta
- * @param string $proveedor El nombre del proveedor
- * @return bool true en caso de éxito o false en caso de error
- */
-public function isPlantasAsociadasAlUsuario($usuarioId, $idPlanta, $proveedor) {
-    try {
-        $conexion = Conexion::getInstance();
-        $conn = $conexion->getConexion();
+     * Verificar si una planta está asociada a un usuario.
+     * 
+     * @param int $usuarioId El ID del usuario
+     * @param int $idPlanta El ID de la planta
+     * @param string $proveedor El nombre del proveedor
+     * @return bool true en caso de éxito o false en caso de error
+     */
+    public function isPlantasAsociadasAlUsuario($usuarioId, $idPlanta, $proveedor)
+    {
+        try {
+            $conexion = Conexion::getInstance();
+            $conn = $conexion->getConexion();
 
-        $query = "SELECT * FROM plantas_asociadas WHERE usuario_id = ? AND planta_id = ? AND proveedor_id = (SELECT proveedores.id from proveedores WHERE proveedores.nombre = ?);";
-        $stmt = $conn->prepare($query);
-        if (!$stmt) {
-            throw new Exception("Error en la preparación de la consulta: " . $conn->error);
+            $query = "SELECT * FROM plantas_asociadas WHERE usuario_id = ? AND planta_id = ? AND proveedor_id = (SELECT proveedores.id from proveedores WHERE proveedores.nombre = ?);";
+            $stmt = $conn->prepare($query);
+            if (!$stmt) {
+                throw new Exception("Error en la preparación de la consulta: " . $conn->error);
+            }
+
+            // Vincula los parámetros
+            $stmt->bind_param('iss', $usuarioId, $idPlanta, $proveedor);
+
+            // Ejecuta la consulta
+            if (!$stmt->execute()) {
+                throw new Exception("Error en la ejecución de la consulta: " . $stmt->error);
+            }
+
+            // Recoge el resultado
+            $result = $stmt->get_result();
+
+            // Devuelve true si se encontró una fila, false en caso contrario
+            $existeAsociacion = $result->num_rows > 0;
+
+            // Cierra la consulta y la conexión
+            $stmt->close();
+
+            return $existeAsociacion;
+        } catch (Exception $e) {
+            error_log("Error al verificar la asociación entre usuario y planta: " . $e->getMessage());
+            return false;
         }
-
-        // Vincula los parámetros
-        $stmt->bind_param('iss', $usuarioId, $idPlanta, $proveedor);
-
-        // Ejecuta la consulta
-        if (!$stmt->execute()) {
-            throw new Exception("Error en la ejecución de la consulta: " . $stmt->error);
-        }
-
-        // Recoge el resultado
-        $result = $stmt->get_result();
-
-        // Devuelve true si se encontró una fila, false en caso contrario
-        $existeAsociacion = $result->num_rows > 0;
-
-        // Cierra la consulta y la conexión
-        $stmt->close();
-
-        return $existeAsociacion;
-
-    } catch (Exception $e) {
-        error_log("Error al verificar la asociación entre usuario y planta: " . $e->getMessage());
-        return false;
     }
-} 
 }
-
-?>
