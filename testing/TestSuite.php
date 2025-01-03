6a5de2b52b82fd05a13263799c26d6bd386bf6d0
nestorsan77 <nestorsan77@gmail.com>
2025-01-03 20:06:40 +0100

    Creado mas testing en JUnit de los servicios de VictronEnergy y las alertas de las plantas de VictronEnergy

--- testing/TestSuite.php
+++ testing/TestSuite.php
@@ -2,5 +2,6 @@
 
 // Incluir los tests específicos
 require_once __DIR__ . '/LogsEndpointTest.php';
+require_once __DIR__ . '/PlantAlert.php';
 
 // Si necesitas agregar más pruebas, simplemente las incluyes aquí.
