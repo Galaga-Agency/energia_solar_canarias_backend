533453bcc01fe529b163a47e6355a02436663a21
nestorsan77 <nestorsan77@gmail.com>
2025-04-07 12:14:43 +0100

    cambios de la api que nos proporciona las coordenadas Nominatim para que no de errores

--- app/models/Nominatim.php
+++ app/models/Nominatim.php
@@ -1,34 +1,36 @@
 <?php
-class Nominatim{
-//Esta api es para sacar las coordenadas de la ubicación
-//ejemplo https://nominatim.openstreetmap.org/search?q=Los+Parrales+5,+Arucas,+Canary+Islands,+Spain&format=json
-public function devolverCoordenadas($localizacion){
-    // URL de Nominatim para obtener coordenadas
-    $nominatimUrl = "https://nominatim.openstreetmap.org/search?q=" . urlencode($localizacion) . "&format=json&limit=1";
+class Nominatim
+{
+    //Esta api es para sacar las coordenadas de la ubicación
+    //ejemplo https://nominatim.openstreetmap.org/search?q=Los+Parrales+5,+Arucas,+Canary+Islands,+Spain&format=json
+    public function devolverCoordenadas($localizacion)
+    {
+        // URL de Nominatim para obtener coordenadas
+        $nominatimUrl = "https://nominatim.openstreetmap.org/search?q=" . urlencode($localizacion) . "&format=json&limit=1";
 
-    $options = [
-        "http" => [
-            "header" => "User-Agent: MyApp/1.0 (your_email@example.com)"
-        ]
-    ];
+        $options = [
+            "http" => [
+                "header" => "User-Agent: ESC-Backend/1.0 (soporte@galagaagency.com)"
+            ]
+        ];
+        
 
-    $context = stream_context_create($options);
+        $context = stream_context_create($options);
 
-    // Realizar la solicitud a Nominatim
-    $nominatimResponse = file_get_contents($nominatimUrl, false, $context);
+        // Realizar la solicitud a Nominatim
+        $nominatimResponse = file_get_contents($nominatimUrl, false, $context);
 
-    if ($nominatimResponse === FALSE) {
-        die("Error al conectarse a Nominatim.");
+        if ($nominatimResponse === FALSE) {
+            die("Error al conectarse a Nominatim.");
+        }
+
+        // Decodificar la respuesta JSON de Nominatim
+        $nominatimData = json_decode($nominatimResponse, true);
+
+        if (!isset($nominatimData[0])) {
+            die("No se encontraron coordenadas para la ubicación especificada.");
+        }
+        return $nominatimData;
     }
-    
-    // Decodificar la respuesta JSON de Nominatim
-    $nominatimData = json_decode($nominatimResponse, true);
-    
-    if (!isset($nominatimData[0])) {
-        die("No se encontraron coordenadas para la ubicación especificada.");
-    }
-    return $nominatimData;
 }
-}
-
 ?>
